e9ad9c94e290e1f1c3d66c62cd259de09abc4072
AleksaSmolovic <aleksa404s@gmail.com>
2020-02-28 09:44:22 +0100

    predavanje

---
+++ AmplitudoObuka_24.02.2020/contact.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    
+    <!-- prihvatanje i ispisivanje GET parametara na stranu -->
+    <h1><i><?php echo 'Ime i prezime: ' . $_GET['name'] . " " . $_GET['surname'] ?><i></h1>
+
+</body>
+</html>
